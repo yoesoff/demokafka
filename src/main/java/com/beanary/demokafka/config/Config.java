48b1f2537679d8ed5e62662fd53ff313305087d1
Mh Yusuf <mhyusufibrahim@gmail.com>
2020-05-12 16:48:13 +0700

    Gson

--- src/main/java/com/beanary/demokafka/config/Config.java
+++ src/main/java/com/beanary/demokafka/config/Config.java
@@ -1,11 +1,14 @@
 package com.beanary.demokafka.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.gson.Gson;
 
 @Configuration
 public class Config {
+	
+	@Bean
 	public Gson jsonConverter() {
 		return new Gson();
 	}
